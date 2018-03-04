aa9de420614379136fd7808bd7dc0690103ecf1f
Nguyen Trung Long <somethings.ttt@gmail.com>
2018-03-04 17:38:07 +0700

    commit

---
+++ Tính chiều cao/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Tính_chiều_cao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputMain = new System.Windows.Forms.TextBox();
+            this.txtMain = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vnmItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enlItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.inputMain);
+            this.panel1.Controls.Add(this.txtMain);
+            this.panel1.Location = new System.Drawing.Point(12, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(446, 39);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14F);
+            this.label1.Location = new System.Drawing.Point(314, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "cm";
+            // 
+            // inputMain
+            // 
+            this.inputMain.Location = new System.Drawing.Point(217, 10);
+            this.inputMain.Name = "inputMain";
+            this.inputMain.Size = new System.Drawing.Size(100, 20);
+            this.inputMain.TabIndex = 1;
+            // 
+            // txtMain
+            // 
+            this.txtMain.AutoSize = true;
+            this.txtMain.Font = new System.Drawing.Font("Arial", 14F);
+            this.txtMain.Location = new System.Drawing.Point(3, 9);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(221, 22);
+            this.txtMain.TabIndex = 0;
+            this.txtMain.Text = "Nhập chiều cao của bạn: ";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(470, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vnmItem,
+            this.enlItem});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // vnmItem
+            // 
+            this.vnmItem.Name = "vnmItem";
+            this.vnmItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.V)));
+            this.vnmItem.Size = new System.Drawing.Size(191, 22);
+            this.vnmItem.Text = "Tiếng Việt";
+            this.vnmItem.Click += new System.EventHandler(this.vnmItem_Click);
+            // 
+            // enlItem
+            // 
+            this.enlItem.Name = "enlItem";
+            this.enlItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.E)));
+            this.enlItem.Size = new System.Drawing.Size(191, 22);
+            this.enlItem.Text = "English";
+            this.enlItem.Click += new System.EventHandler(this.enlItem_Click);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(367, 45);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(88, 23);
+            this.btn.TabIndex = 3;
+            this.btn.Text = "Submit";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8F);
+            this.label2.Location = new System.Drawing.Point(304, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Copyright by Nguyễn Trung Long";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 95);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Phần mềm tính chiều cao";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputMain;
+        private System.Windows.Forms.Label txtMain;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vnmItem;
+        private System.Windows.Forms.ToolStripMenuItem enlItem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tính chiều cao/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tính_chiều_cao
+{
+    public partial class Form1 : Form
+    {
+        public static string s, t, l = "vnm";//s là string, t là title, l là language
+        private void vnmItem_Click(object sender, EventArgs e)
+        {
+            l = "vnm";
+            this.txtMain.Text = "Nhập chiều cao của bạn";
+            this.Text = "Phần mềm tính chiều cao";
+        }
+
+        private void enlItem_Click(object sender, EventArgs e)
+        {
+            l = "enl";
+            this.txtMain.Text = "Press your height: ";
+            this.Text = "Height calculator app";
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            int temp;
+            if (l == "vnm")
+            {
+                if (int.TryParse(this.inputMain.Text, out temp) == false)
+                {
+                    s = "Giá trị nhập sai, thử lại!";
+                    t = "Lỗi";
+                }
+                else if (int.Parse(this.inputMain.Text) <= 30 || int.Parse(this.inputMain.Text) >= 260)
+                {
+                    s = "Có gì đó sai sai. Nhập lại đê!";
+                    t = "Hư cấu time";
+                }
+                else
+                {
+                    s = "Chiều cao của bạn là: " + this.inputMain.Text + "cm";
+                    t = "Kết quả";
+                }
+                MessageBox.Show(this, s, t, MessageBoxButtons.OK);
+            }
+            else
+            {
+                if (int.TryParse(this.inputMain.Text, out temp) == false)
+                {
+                    s = "Wrong value, try again!";
+                    t = "Error";
+                }
+                else if (int.Parse(this.inputMain.Text) <= 30 || int.Parse(this.inputMain.Text) >= 260)
+                {
+                    s = "Somethings went wrong with that value, try again!";
+                    t = "Error";
+                }
+                else
+                {
+                    s = "Your height is: " + this.inputMain.Text + "cm";
+                    t = "Result";
+                }
+                MessageBox.Show(this, s, t, MessageBoxButtons.OK);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            float size = (float)Math.Round((this.Width / 35.37) * 10) / 10;
+            this.txtMain.Font = new Font("arial", size);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
